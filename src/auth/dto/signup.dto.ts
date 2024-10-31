ed22d1929c5599ec09700aa96986186fb3988e4f
cyvac1 <cyvac1@jinhakapply.com>
2024-10-31 09:01:14 +0900

    ✨ SignUpDto 정의

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,14 @@
+// src/auth/dto/signup.dto.ts
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  password: string;
+}
