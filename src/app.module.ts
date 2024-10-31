d80736ab0bfbd67b618c0291cadf23e3541a8aac
cyvac1 <cyvac1@jinhakapply.com>
2024-10-31 09:01:17 +0900

    🎨 글로벌 logging 인터셉터 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
