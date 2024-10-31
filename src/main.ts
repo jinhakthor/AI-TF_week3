e7fc743441d57248279c0b429e90923699d57ea4
cyvac1 <cyvac1@jinhakapply.com>
2024-10-31 09:01:13 +0900

    ✨ 글로벌 Validation 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+// src/main.ts
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 전역 Validation Pipe 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 있으면 에러 발생
+      transform: true, // 요청 데이터를 자동으로 변환
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
